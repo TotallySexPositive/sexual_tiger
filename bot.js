df028367a493b04f19e5c27e8c840252768bac55
Steve <rieger07@gmail.com>
2018-01-30 18:42:38 -0500

    using discord.js

--- bot.js
+++ bot.js
@@ -1,53 +1,15 @@
+const Discord = require('discord.js');
+const auth = require('./auth.json')
+const client = new Discord.Client();
 
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
+client.on('ready', () => {
+    console.log('I am ready!');
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+
+client.on('message', message => {
+    if (message.content==='ping'){
+        message.reply('pong');
+    };
 });
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-	if(message.author.bot) return;
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-                break;
-            case 'block':
-		bot.sendMessage({
-                    to: channelID,
-                    message: 'Blocking!'
-                });
-            	var currentTime = new Date().getTime();
-		while (currentTime + 5000 >= new Date().getTime()) {}
-		bot.sendMessage({
-                    to: channelID,
-                    message: 'Done blocking!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
-});
+
+client.login(auth.token)
