6c820c8605285de529fbd79b9bf32e46671d6495
Adam <adam.r.wentzel@gmail.com>
2018-02-01 15:19:27 -0500

    added error handling

--- commands/vup.js
+++ commands/vup.js
@@ -1,5 +1,15 @@
 exports.run = (client, message, args) => {
-    let dispatcher = message.member.voiceChannel.connection.dispatcher
+    let vc = message.member.voiceChannel
+    if(vc === undefined) {
+        message.channel.send("I'm not even in a channel.")
+        return;
+    }
+    let dispatcher = vc.connection.dispatcher
+    if(dispatcher === undefined) {
+        message.channel.send("No audio is playing.  You must be hearing things.")
+        return;
+    }
+    
     let current_volume = dispatcher.volume;
     if(current_volume >= .5) {
         message.channel.send("This is plenty loud enough.")
