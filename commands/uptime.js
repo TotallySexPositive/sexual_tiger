b71a152b748f13eb834a5ce41f792968f42a1338
Steve <rieger07@gmail.com>
2018-02-01 01:11:03 -0500

    time... i hate time..

--- commands/uptime.js
+++ commands/uptime.js
@@ -4,19 +4,16 @@ exports.run = (client, message, args) => {
 }
 
 function timeConversion(millisec){
+    var secs = (millisec / 1000).toFixed(0);
+    days = Math.floor(secs/86400)
+    hours = Math.floor(secs/3600) % 24
+    minutes = Math.floor(secs/60)%60
+    seconds = secs%60
 
-    var seconds = (millisec / 1000).toFixed(0);
-
-    var minutes = (millisec / (1000 * 60)).toFixed(0);
-
-    var hours = (millisec / (1000 * 60 * 60)).toFixed(0);
-
-    var days = (millisec / (1000 * 60 * 60 * 24)).toFixed(0);
-
-    seconds = (""+(seconds%60)).padStart(2,"0")
-    minutes = (""+(minutes%60)).padStart(2,"0")
-    hours = (""+(hours%24)).padStart(2,"0")
-    days = days.padStart(2,"0");
+    seconds = (""+seconds).padStart(2,"0")
+    minutes = (""+(minutes)).padStart(2,"0")
+    hours = (""+(hours)).padStart(2,"0")
+    days = (""+days).padStart(2,"0");
 
     return `${days} days ${hours}:${minutes}:${seconds}`;
 }
