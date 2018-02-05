952a198bcd7d2c504ec62380acee7e7042e1dac2
Steve <rieger07@gmail.com>
2018-02-05 16:46:08 -0500

    yay things

---
+++ commands/hoi.js
@@ -0,0 +1,6 @@
+const path = require("path");
+const reg = require("./people.json")
+exports.run = (client, message, args) => {
+	  message.channel.send("",{"files": ["https://funnypictures1.fjcdn.com/pictures/Hoi+im+temmie_36051e_5725743.jpg"]})
+		
+}
