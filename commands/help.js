474ec81acec1de6f3550f607af8550c668495b83
Steve <rieger07@gmail.com>
2018-02-05 19:15:25 -0500

    yay!

--- commands/help.js
+++ commands/help.js
@@ -1,18 +1,42 @@
-var fs = require('fs');
-var path = require('path');
+let fs = require('fs');
+let path = require('path');
+let cfg = require('../configure.json')
+let asciitable = require("asciitable")
 exports.run = (client, message, args) => {
+
+    let options = {
+        skinny: true,
+        intersectionCharacter: "+",
+        columns: [
+            {field: "command", name: "Command"},
+            {field: "halp",    name: "Details"}
+        ],
+    };
+
     p  = path.resolve("commands");
     console.log(p);
     fs.readdir(p, (err,items)=>{
-            console.log(items);
-	    var mes = "Available commands: \n";
+	    let mes = "Available commands: \n";
+        let rows = [];
             items.forEach((item)=>{
-                console.log(item);
                 if(!item.endsWith('.js'))return;
-		mes = mes + "\t!" + item.replace(".js","") + "\n";
-	    })
 
-	    var m = `\`\`\`${mes}\`\`\``;
-	    message.channel.send(m).catch(console.error);
-    })
-}
+                script = path.resolve("commands", item);
+                temp = require(script);
+                k = Object.keys(temp);
+                
+                let cmd = `${cfg.prefix}${item.replace(".js","")}`
+                if (k.includes("help")){
+                    hlp = `${temp.help()}`;
+                    
+                } else{
+                    hlp = "";
+                }
+                rows.push({"command": cmd, "halp":hlp})
+	        });
+
+        let table = asciitable(options, rows);
+	    
+	    message.channel.send(table,{"split":true, "code":true}).catch((err)=>{console.error(err.message);console.log(table)});
+    });
+};
--- commands/reload.js
+++ commands/reload.js
@@ -1,8 +1,27 @@
 const sanitize = require("sanitize-filename");
+const path = require("path")
+const fs = require("fs")
+
 exports.run = (client, message, args) => {
     if(!args || args.length < 1 || args.length == undefined) return message.reply("Must provide a command name to reload.");
     // the path is relative to the *current folder*, so just ./filename.js
-    var f = sanitize(args[0]); //sanitize that shit
-    delete require.cache[require.resolve(`./${f}.js`)];
-    message.reply(`The command ${f} has been reloaded`);
+    if(args[0]==="all"){
+    	//reload all modules
+    	let p = path.resolve("commands")
+    	fs.readdir(p, (err,items)=>{
+    		if(err){return message.channel.send(err.message)}
+    		items.forEach((item)=>{
+    			if(!item.endsWith('.js'))return;
+			    delete require.cache[require.resolve(`./${item}`)];
+			    console.log(item)
+    		})
+    		message.reply(`Reloaded all commands.`);
+    	})
+    	
+    } else{
+    	var f = sanitize(args[0]); //sanitize that shit
+	    delete require.cache[require.resolve(`./${f}.js`)];
+	    message.reply(`The command ${f} has been reloaded`);
+    }
+    
 };
