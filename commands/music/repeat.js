661dd520e933854d8edb7360ab90ad08237ea022
Steve <rieger07@gmail.com>
2018-02-05 23:22:27 -0500

    can't declare variables in inline statements

--- commands/music/repeat.js
+++ commands/music/repeat.js
@@ -1,8 +1,11 @@
 //TODO Switch this to db when adam commits the createtable stuff and playlist stuff
 exports.run = (client, message, args) => {
     global.repeat = !global.repeat;
-    if (global.repeat) let m = "Sounds will repeat";
-    else let m="Sounds will not repeat";
+    if (global.repeat){
+    	let m = "Sounds will repeat";	
+    } else{
+    	let m="Sounds will not repeat";
+    }
 
     message.channel.send(m)
 };
