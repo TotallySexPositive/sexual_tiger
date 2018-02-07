1874bf9398a59e973e3cfb8fe76e3b8bc0028c94
Steve <rieger07@gmail.com>
2018-02-06 23:47:30 -0500

    added requests, added upload deleted audio folder

---
+++ commands/music/upload.js
@@ -0,0 +1,24 @@
+const {spawn} = require('child_process');
+const path = require("path");
+const request = require("request");
+const fs = require("fs")
+
+exports.run = (client, message, args) => {
+    let attachments = message.attachments.array()
+    if (attachments.length<0)return message.channel.send("Need to actually attach something");
+	message.channel.send(`Downloading ${attachments.length} attachments.`);
+
+    attachments.forEach(item=>{
+        fpath = path.resolve("uploaded_audio",item.filename)
+        request(item.url)
+            .pipe(fs.createWriteStream(fpath))
+            .on('finish', (ok)=>{console.log(ok)})
+            .on('error', (err)=>{console.error(err)})
+
+    })
+	
+}
+
+exports.help = () =>{
+    return "Upload a music file!";
+};
