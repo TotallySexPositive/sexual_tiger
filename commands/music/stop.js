f46d92e2d43ec9018e6904c8c075bcb452e3be53
Adam <adam.r.wentzel@gmail.com>
2018-02-11 15:05:12 -0500

    Fixed bug that crashed bot when  was used during a playlist, increased number of songs returned in search

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -1,9 +1,15 @@
 exports.run = (client, message, args) => {
+    var server = global.servers[message.guild.id];
     let vc = message.member.voiceChannel
     if(vc === undefined) {
         message.channel.send("I'm not even in a channel.")
         return;
     }
+    
+    server.songs = [];
+    server.current_song_index = -1;
+    server.current_song = {};
+
     message.guild.voiceConnection.disconnect();
     /*let dispatcher = vc.connection.dispatcher
     if(dispatcher === undefined) {
