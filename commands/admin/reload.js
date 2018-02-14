1a4edcf90d099e156916bf9f6d744475535d4339
Steve <rieger07@gmail.com>
2018-02-13 21:13:23 -0500

    will walk through subdirs to find your command

--- commands/admin/reload.js
+++ commands/admin/reload.js
@@ -1,39 +1,34 @@
 const sanitize = require("sanitize-filename");
 const path = require("path")
-const fs = require("fs")
+const dir = require("node-dir")
 
 exports.run = (client, message, args) => {
     if(!args || args.length < 1 || args.length == undefined) return message.reply("Must provide a command name to reload.");
     // the path is relative to the *current folder*, so just ./filename.js
     if(args[0]==="all"){
         //reload all modules
-        global.commandTypes.forEach(i=>{
-            let p = path.resolve("commands", i);
-
-            fs.readdir(p, (err,items)=>{
-                if(err){return message.channel.send(err.message)}
-                items.forEach((item)=>{
-                    if(!item.endsWith('.js'))return;
-                    delete require.cache[path.resolve("commands", i, item)];
-                    console.log(item)
-                })
-                
+        dir.files(path.resolve("commands"), (err, files)=>{
+            if(err)console.error(err);
+            files.forEach((filename)=>{
+                if(!filename.endsWith(".js"))return;
+                console.log(filename)
+                delete require.cache[filename];
             })
         })
         message.reply(`Reloaded all commands.`);
         
     } else{
-        global.commandTypes.some((i)=>{
-            p = path.resolve("commands", i, args[0]+".js")
-            if (fs.existsSync(p)){
-                delete require.cache[p];
-                message.reply(`The command ${args[0]} has been reloaded`);
-            }else{
-                //message.reply(`Fuck you, ${p} didn't exist.`)
-            }
-            
+        dir.files(path.resolve("commands"), (err, files)=>{
+            if(err)console.error(err);
+            files.some((filename)=>{
+                if(filename.endsWith(".js") && filename.includes(args[0])){
+                    console.log(filename)
+                    delete require.cache[filename];
+                    return true;
+                }else return false;
+            })
         })
-        
+        message.reply(`Reloaded ${args[0]}.`);
     }
     
 };
