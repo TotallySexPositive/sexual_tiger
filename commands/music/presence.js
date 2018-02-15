5baf86fe3805b8bd5f457dba43f6d180b93a0970
Adam <adam.r.wentzel@gmail.com>
2018-02-15 01:09:53 -0500

    Made presence display message indicating its current state

--- commands/music/presence.js
+++ commands/music/presence.js
@@ -1,10 +1,14 @@
 exports.run =  (client, message, args)=>{
-	let server = global.servers[message.guild.id]
-	let p = server.maintain_presence
-	console.log(p)
-	let not_p = !server.maintain_presence
-	console.log(not_p)
-	server.maintain_presence = not_p
+	let server = global.servers[message.guild.id];
+    server.maintain_presence = !server.maintain_presence;
+    let m = ""
+    if (server.maintain_presence){
+    	m = "Ok, I'll hang out for a bit.";	
+    } else{
+    	m = "Dont worry, I'll fucking drop you like a shit covered potato.";
+    }
+
+    message.channel.send(m)
 	//Could make it leave here if you want
 }
 
