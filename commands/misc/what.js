255456cae6991d70735362ae1ff7d5b0a0f8b915
Adam <adam.r.wentzel@gmail.com>
2018-02-15 01:08:07 -0500

    just testing shit

--- commands/misc/what.js
+++ commands/misc/what.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const DAL = require(path.resolve("dal.js"))
+const UTIL = require(path.resolve("utils.js"))
 
 exports.run = (client, message, args) => {
     
@@ -7,60 +8,8 @@ exports.run = (client, message, args) => {
     var terms = args.slice(1).join(" ");
 
     switch(head) {
-        case "song_id":
-            DAL.findSongById(terms, (err, song) =>{
-                if(err) {
-                    message.channel.send(`Query Failed. ${err.message}`)
-                } else if(song === undefined) {
-                    message.channel.send(`No song with that ID.`)
-                } else {
-                    message.channel.send(`Found Song: ${song.name}`)
-                }
-            });
-            break;
-        case "playlist_id":
-            DAL.findPlaylistById(terms, (err, playlist) =>{
-                if(err) {
-                    message.channel.send("Query Failed.")
-                } else if(playlist === undefined) {
-                    message.channel.send(`No playlist with that ID.`)
-                } else {
-                    message.channel.send(`Found playlist: ${playlist.name}`)
-                }
-            });
-            break;
-        case "playlist_name":
-            DAL.findPlaylistByName(terms, (err, playlist) =>{
-                if(err) {
-                    message.channel.send("Query Failed.")
-                } else if(playlist === undefined) {
-                    message.channel.send(`No playlist with that Name.`)
-                } else {
-                    message.channel.send(`Found playlist: ${playlist.playlist_id}`)
-                }
-            });
-            break;
-        case "playlist_songs_id":
-            DAL.getSongsByPlaylistId(terms, (err, songs) =>{
-                if(err) {
-                    message.channel.send("Query Failed.")
-                } else if(!songs.length) {
-                    message.channel.send(`No Songs on playlist.`)
-                } else {
-                    message.channel.send(`Found Songs: ${songs}`)
-                }
-            });
-            break;
-        case "playlist_songs_name":
-            DAL.getSongsByPlaylistName(terms, (err, songs) =>{
-                if(err) {
-                    message.channel.send("Query Failed.")
-                } else if(!songs.length) {
-                    message.channel.send(`No Songs on playlist.`)
-                } else {
-                    message.channel.send(`Found Songs: ${songs}`)
-                }
-            });
+        case "test":
+            UTIL.print(terms)
             break;
         default:
             message.channel.send("Invalid thingy passed.")
