f1a196dbe84510fe0803f62331c682703adf1029
Adam <adam.r.wentzel@gmail.com>
2018-02-18 00:11:55 -0500

    Fixed by when resuming nothing, improved handling and response of resume command. Fixes #45

--- commands/music/resume.js
+++ commands/music/resume.js
@@ -1,11 +1,11 @@
 exports.run = (client, message, args) => {
     let vc = message.member.voiceChannel
-    if(vc === undefined) {
+    if(!vc || !vc.connection) {
         message.channel.send("I'm not even in a channel.")
         return;
     }
     let dispatcher = vc.connection.dispatcher
-    if(dispatcher === undefined) {
+    if(!dispatcher || !dispatcher.paused) {
         message.channel.send("Nothing to resume.")
         return;
     }
