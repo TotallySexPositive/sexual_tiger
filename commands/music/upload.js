9590d7f7328a090bfd7521de977f38fadd9c2f6e
Adam <adam.r.wentzel@gmail.com>
2018-02-19 01:28:53 -0500

    Made uploaded audio files get processed right away,  isnt needed by normal users now

--- commands/music/upload.js
+++ commands/music/upload.js
@@ -1,22 +1,23 @@
-const {spawn} = require('child_process');
-const path = require("path");
-const request = require("request");
-const fs = require("fs")
+const {spawn}   = require('child_process');
+const path      = require("path");
+const request   = require("request");
+const fs        = require("fs")
+const UTIL      = require(path.resolve("utils.js"));
 
 exports.run = (client, message, args) => {
     let attachments = message.attachments.array()
-    if (attachments.length<0)return message.channel.send("Need to actually attach something");
+    if (attachments.length < 0) return message.channel.send("Need to actually attach something");
 	message.channel.send(`Downloading ${attachments.length} attachments.`);
 
-    attachments.forEach(item=>{
+    attachments.forEach(item => {
         fpath = path.resolve(global.audio_dirs.uploaded,item.filename)
         request(item.url)
             .pipe(fs.createWriteStream(fpath))
-            .on('finish', () => {/*good*/})
+            .on('finish', () => {
+                UTIL.processAudioFile(fpath, null, message);
+            })
             .on('error', (err) => {console.error(err)})
-
     })
-	
 }
 
 exports.help = () =>{
