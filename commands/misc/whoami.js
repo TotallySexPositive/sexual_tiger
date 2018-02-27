21db23132d5b0ba52f858d959f2fdfcd595565af
Adam <adam.r.wentzel@gmail.com>
2018-02-27 02:13:38 -0500

    Fixed bug in reaction delete.

--- commands/misc/whoami.js
+++ commands/misc/whoami.js
@@ -1,7 +1,14 @@
+const path  = require("path");
+const fs    = require("fs");
+const DAL   = require(path.resolve("dal.js"))
+const UTIL  = require(path.resolve("utils.js"))
+
 exports.run = (client, message, args) => {
     let author = message.author;
     let member = message.member;
 
+    let is_admin = UTIL.isAdmin(member);
+    
     let description = `
     User id: ${author.id}
     Username: ${author.username}
@@ -9,7 +16,9 @@ exports.run = (client, message, args) => {
     Display Name: ${member.displayName}
     Discriminator: ${author.discriminator}
     On Server: ${message.guild.id}
+    isAdmin: ${is_admin}
     `.replace(/\n +/g, `\n`);
+
 
     console.log(member.roles)
     let roles = member.roles;
