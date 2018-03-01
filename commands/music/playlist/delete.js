97a2c3ad85b64a6072097889dc3e84c3b959abc2
CappuccinoJack <36948201+CappuccinoJack@users.noreply.github.com>
2018-03-01 02:12:41 -0500

    Fixes #32

--- commands/music/playlist/delete.js
+++ commands/music/playlist/delete.js
@@ -6,14 +6,14 @@ exports.run = (client, message, args) => {
     var identifier  = args.join(" ");
     let playlist_id = undefined;
 
-    if(DAL.isInt(identifier)) { //Identifier is an integer, so it must be an id
+    if(DAL.isInt(identifier)) {          //Identifier is an integer, so it must be an id
         playlist_id = identifier;
-    } else { //Identifier is not an Integer, so it must be the name of a playlist, lets find it.
+    } else {                            //Identifier is not an Integer, so it must be the name of a playlist, lets find it.
         var {err, playlist} = DAL.findPlaylistByName(identifier);
         if(err) {
             return message.channel.send(`Sorry, ${message.author.username}, it seems something unexpected happened.`);
         } else if(playlist === undefined) {
-            return message.channel.send(`Sorry, ${message.author.username}, a playlist with that name doesnt exist.`);
+            return message.channel.send(`Sorry, ${message.author.username}, a playlist with that name doesn't exist.`);
         } else {
             playlist_id = playlist.playlist_id;
         }
@@ -24,8 +24,8 @@ exports.run = (client, message, args) => {
     if(err) {
         message.channel.send(`Sorry, ${message.author.username}, it seems something unexpected happened.`);
     } else if (info.changes == 0) { 
-        message.channel.send(`Sorry, ${message.author.username}, but that playlist didnt exist.`);
+        message.channel.send(`Sorry, ${message.author.username}, but that playlist didn't exist.`);
     } else {
-        message.channel.send(`The playlist ${identifier} has been deleted, You're DJ career is over, ${message.author.username}!`);
+        message.channel.send(`The playlist ${identifier} has been deleted, your DJ career is over, ${message.author.username}!`);
     }
 };
