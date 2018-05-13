ec366c2f4b28ac6a0b6c13faa3a3e7261fe26398
Siberian Tiger <tiger@sexualtiger.com>
2018-05-13 01:53:56 -0400

    Fix/finish gifs command

--- commands/fun/gifs.js
+++ commands/fun/gifs.js
@@ -5,24 +5,27 @@ var giphy   = require('giphy-api')(auth.giphy);
 
 exports.run = (client, message, args) => {
 
-    // Search with options using callback
-    giphy.search({
-        q: args.join(" "),
-        limit: 1,
-        offset: Math.floor(Math.random() * 200) + 1
-    }, function (err, res) {
-        if(err) {
-            console.log(err);
-            return message.channel.send("Error while searching for gif.")
-        } else {
-            console.log(res);
-            if(res.data && res.data.images && res.data.images.original) {
-                return message.channel.send(res.data.images.original.url);
+    if(args.length){
+        giphy.search({
+            q: args.join(" "),
+            limit: 1,
+            offset: Math.floor(Math.random() * 200) + 1
+        }, function (err, res) {
+            if(err) {
+                console.log(err);
+                return message.channel.send("Error while searching for gif.")
             } else {
-                return message.channel.send("Couldnt find anything with those tags.")
+                if(res.data && res.data.length ) {
+                    return message.channel.send(res.data[0].url);
+                } else {
+                    return message.channel.send("Couldnt find anything with those terms.")
+                }
             }
-        }
-    });
+        });
+    } else {
+        return message.channel.send("You have to search for something.")
+    }
+    
 }
 
 exports.help = () =>{
@@ -34,14 +37,14 @@ exports.docs = () => {
         tab: "image",
         link: "Pictures",
         parent: "",
-        full_command: "gif",
-        command: "gif",
-        description: "Grab a random image from Giphy with the specified tags, or completely random if no tags",
-        syntax: 'gif [tags]',
+        full_command: "gifs",
+        command: "gifs",
+        description: "Grab a random image from Giphy with the requested search terms",
+        syntax: 'gifs [words to search]',
         examples: [
             {
                 description: "Post image of jumping",
-                code: `gif jump`
+                code: `gifs jump`
             }
         ]
     }
