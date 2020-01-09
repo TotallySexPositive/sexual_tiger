66d1da37708f0fe0dca6ae29f1650b19e98f02bc
Siberian Tiger <tiger@sexualtiger.com>
2020-01-08 23:39:12 -0500

    Update xckd to use new different package with black jack and hookers

--- commands/fun/xkcd.js
+++ commands/fun/xkcd.js
@@ -2,43 +2,67 @@ const path  = require("path");
 const UTIL  = require(path.resolve("utils.js"))
 const auth  = require(path.resolve('auth.json'));
 const parser    = require('yargs-parser')
-const fetch = require('node-fetch');
+const xkcd      = require('xkcd-api');
+const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
 
     let arg_string  = message.content.slice(5); //Chop off $xkcd
     let comic_id    = arg_string.trim()
-    let xkcd_url = "https://xkcd.com/info.0.json"
 
-    if(comic_id) {
-        xkcd_url = `https://xkcd.com/${comic_id}/info.0.json`
-    }
+    let is_int = UTIL.isInt(comic_id);
 
-    fetch(xkcd_url)
-    .then(res => {
-        if(res.ok) {
-            return res.json()
-        }
-        else
+    if(comic_id === "")
+    {
+        xkcd.latest(function(error, response) 
         {
-            return {
-                "month": "9", 
-                "num": 2200, 
-                "link": "", 
-                "year": "2019", 
-                "news": "", 
-                "safe_title": "Unreachable State", 
-                "transcript": "", 
-                "alt": "ERROR: We've reached an unreachable state. Anything is possible. The limits were in our heads all along. Follow your dreams.", 
-                "img": "https://imgs.xkcd.com/comics/unreachable_state.png", 
-                "title": "Unreachable State", 
-                "day": "9"
+            if (error) {
+              console.error(error);
+            } else {
+                const exampleEmbed = new Discord.RichEmbed()
+                .setColor('GOLD')
+                .setTitle(response.title)
+                .setImage(response.img)
+                .setFooter(response.alt);
+                
+                message.channel.send(exampleEmbed);
             }
-        }
-    }).then(json => {
-        message.channel.send(`${json.title}\n${json.img}`);
-        message.channel.send(`\`\`\`${json.alt}\`\`\``);
-    });
+        });
+    }
+    else if(UTIL.isInt(comic_id))
+    {
+        xkcd.get(comic_id, function(error, response) 
+        {
+            if (error) {
+              console.error(error);
+            } else {
+                const exampleEmbed = new Discord.RichEmbed()
+                .setColor('GOLD')
+                .setTitle(response.title)
+                .setImage(response.img)
+                .setFooter(response.alt);
+                
+                message.channel.send(exampleEmbed);
+            }
+        });
+    }
+    else
+    {
+        xkcd.random(function(error, response) 
+        {
+            if (error) {
+              console.error(error);
+            } else {
+                const exampleEmbed = new Discord.RichEmbed()
+                .setColor('GOLD')
+                .setTitle(response.title)
+                .setImage(response.img)
+                .setFooter(response.alt);
+                
+                message.channel.send(exampleEmbed);
+            }
+        });
+    }
 }
 
 
@@ -68,3 +92,36 @@ exports.docs = () => {
     }
     return docs;
 };
+
+
+/*let xkcd_url = "https://xkcd.com/info.0.json"
+
+    if(comic_id) {
+        xkcd_url = `https://xkcd.com/${comic_id}/info.0.json`
+    }
+
+    fetch(xkcd_url)
+    .then(res => {
+        if(res.ok) {
+            return res.json()
+        }
+        else
+        {
+            return {
+                "month": "9", 
+                "num": 2200, 
+                "link": "", 
+                "year": "2019", 
+                "news": "", 
+                "safe_title": "Unreachable State", 
+                "transcript": "", 
+                "alt": "ERROR: We've reached an unreachable state. Anything is possible. The limits were in our heads all along. Follow your dreams.", 
+                "img": "https://imgs.xkcd.com/comics/unreachable_state.png", 
+                "title": "Unreachable State", 
+                "day": "9"
+            }
+        }
+    }).then(json => {
+        message.channel.send(`${json.title}\n${json.img}`);
+        message.channel.send(`\`\`\`${json.alt}\`\`\``);
+    });*/
