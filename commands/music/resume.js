e607f2a31ec6676cb0bff59add49afc9ef37befb
Siberian Tiger <tiger@sexualtiger.com>
2020-05-03 16:25:06 -0400

    Improve handling commands from users in voice channels the bot is not in

--- commands/music/resume.js
+++ commands/music/resume.js
@@ -1,26 +1,24 @@
 exports.run = (client, message, args) => {
-    let vc = message.member.voice.channel
-    var server = global.servers[message.guild.id];
+    let vc      = message.member.voice.channel
+    var server  = global.servers[message.guild.id];
     let promise = server.connectionPromise
 
-    if(vc === undefined) {
-        message.channel.send("I'm not even in a channel.")
-        return;
+    if(vc === null) {
+        return message.channel.send("You are not in a voice channel. I'm not going to listen to you.")
     }
-    if (promise === null)
-    {
-        message.channel.send("Nothing to resume.")
-        return;
+    if (promise === null) {
+        return message.channel.send("Nothing to resume.")
     }
-    promise.then(
-        connection=>{
+
+    promise.then(connection => {
+        if(message.member.voice.channel === connection.channel) {
             connection.dispatcher.resume()
+        } else {
+            return message.channel.send("You are in a different voice channel. I'm not going to listen to you.")
         }
-    ).catch(
-        reason=>{
-            console.log(reason)
-        }
-    )
+    }).catch(reason => {
+        console.log(reason)
+    })
 }
 
 exports.help = () =>{
