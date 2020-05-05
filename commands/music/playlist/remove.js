31e9667fff5c6632989febc9a494b3e81ce38ca7
Siberian Tiger <tiger@sexualtiger.com>
2020-05-05 00:31:53 -0400

    Fixes #54 and improves output a little

--- commands/music/playlist/remove.js
+++ commands/music/playlist/remove.js
@@ -1,90 +1,70 @@
 const path = require("path")
 const DAL = require(path.resolve("dal.js"))
+const parser    = require('yargs-parser')
 
 exports.run = (client, message, args) => {
-    
-    let playlist_id = undefined;
-    let song_id     = undefined;
-
-    if(args.length < 2) {
-        return message.channel.send("Invalid use of playlist remove")
-    } else if(args.length == 2) {
-        if(DAL.isInt(args[0])) {
-            playlist_id = args[0];
-        } else {
-            let {err, playlist} = DAL.findPlaylistByName(args[0]);
-            if(err) {
-                return message.channel.send("There was an error while searching for a playlist by that name.")
-            } else if(playlist === undefined) {
-                return message.channel.send("There is no playlist by that name.")
-            } else {
-                playlist_id = playlist.playlist_id;
-            }
-        }
-        if(DAL.isInt(args[1])) {
-            song_id = args[1];
-        } else {
-            let {err, song} = DAL.findSongByName(args[1]);
-            if(err) {
-                return message.channel.send("There was an error while searching for a song by that name.")
-            } else if(song === undefined) {
-                return message.channel.send("There is no song by that name.")
-            } else {
-                song_id = song.song_id;
-            }
-        }
-    } else { //More than 2 args, means we need to rely on the | split between playlist identifier and song identifier
-        if((args.join(" ").match(/\|/g) || []).length !== 1) {
-            return message.channel.send("The playlist identifier and song identifier must be seperated by a single |.")
-        } else {
-            let split_on_pipe = args.join(" ").split("|");
-            let playlist_identifier = split_on_pipe[0].trim();
-            let song_identifier = split_on_pipe[1].trim();
-            
-            if(DAL.isInt(playlist_identifier)) {
-                playlist_id = playlist_identifier;
-            } else {
-                let {err, playlist} = DAL.findPlaylistByName(playlist_identifier);
-                if(err) {
-                    return message.channel.send("There was an error while searching for a playlist by that name.")
-                } else if(playlist === undefined) {
-                    return message.channel.send("There is no playlist by that name.")
-                } else {
-                    playlist_id = playlist.playlist_id;
-                }
-            }
-
-            if(DAL.isInt(song_identifier)) {
-                song_id = song_identifier;
-            } else {
-                let {err2, song} = DAL.findSongByName(song_identifier);
-                if(err2) {
-                    return message.channel.send("There was an error while searching for a song by that name.")
-                } else if(song === undefined) {
-                    return message.channel.send("There is no song by that name.")
-                } else {
-                    song_id = song.song_id;
-                }
-            }
-        }
+    var opts = {
+        alias: {
+            playlist: ['p'],
+            song: ['s'],
+        },
+        configuration: {
+            'short-option-groups': false
+          }
     }
 
-    if(playlist_id === undefined || song_id === undefined) {
-        //This shouldnt be possible by here, but just in ase
-        message.channel.send(`There was an error that shouldnt be possible!`);
+    let arg_string = message.content.slice(13); //Chop off $playlist add
+    var argv = parser(arg_string.replace(/= +/g, "="), opts)
+
+    if(!argv.s || !argv.p) {
+        return message.channel.send('You must provide a playlist and at least 1 song.  IDs or complete names are acceptable.  EX: $playlist remove -p 3 -s "Drop the Ball"')
+    }
+
+    //Process/verify playlist
+    if(Array.isArray(argv.p)) return message.channel.send("You must specify only a single playlist.");
+
+    let {err:p_err, playlist}   = DAL.findPlaylistByIdentifier(argv.p)
+    if(p_err) return message.channel.send(`Oops, issue trying to find the specified playlist, ${p_err.message}`);
+    if(playlist === undefined) return message.channel.send(`The specified playlist doesnt exist.`);
+
+    //Process/verify songs
+    let song_identifiers = [];
+    if(Array.isArray(argv.s)) {
+        song_identifiers = argv.s
     } else {
-        let {err, info} = DAL.removeFromPlaylist(playlist_id, song_id);
-        if(err) {
-            console.log(err);
-            message.channel.send(`Sorry, ${message.author.username}, it seems something unexpected happened.`);
-        } else if (info.changes == 0) {
-            message.channel.send(`That song wasn't on that playlist.`);
-        } else {
-            message.channel.send(`That song has been removed from that playlist.`);
-        }
+        song_identifiers.push(argv.s);
     }
 
-    
+    let skipped_songs = [];
+    let removed_songs = [];
+
+    song_identifiers.forEach((identifier) => {
+        let {err:s_err, song}       = DAL.findSongByIdentifier(identifier)
+
+        if(s_err) {
+            skipped_songs.push(`X_X: ${identifier}`);
+            console.log(s_rr);
+        } else if(song === undefined) {
+            skipped_songs.push(`DNE: ${identifier}`);
+        } else {
+            let {err, info} = DAL.removeFromPlaylist(playlist.playlist_id, song.song_id);
+            if(err) {
+                console.log(err);
+                skipped_songs.push(`X_X: ${song.song_id}: ${song.name}`);
+            } else if(!info.changes) {
+                skipped_songs.push(`NOL: ${song.song_id}: ${song.name}`);
+            } else {
+                removed_songs.push(`${song.name}`);
+            }
+        }
+    });
+
+    if(removed_songs.length) {
+        message.channel.send(`The following songs were removed from **${playlist.name}**:\n${removed_songs.join('\n')}`)
+    }
+    if(skipped_songs.length) {
+        message.channel.send(`The following songs either didnt exist or crashed the query and were not removed:\n${skipped_songs.join('\n')}`)
+    }
 };
 
 exports.help = () =>{
