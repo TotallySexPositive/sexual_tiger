3547b55752b8653ea4989e8524e6081f85d473ee
Adam <narayanjr@yahoo.com>
2021-06-10 20:30:39 -0700

    fix xkcd and refactor

--- src/commands/fun/xkcd.ts
+++ src/commands/fun/xkcd.ts
@@ -3,39 +3,40 @@ import xkcd from "xkcd-api";
 import { Command } from "../../types/Command";
 import * as UTIL from "../../utils";
 
+function post(message: Message, error: any, response: any) {
+	if (error) {
+		console.error(error);
+	} else {
+		const exampleEmbed = new MessageEmbed()
+			.setColor("GOLD")
+			.setTitle(response.title)
+			.setImage(response.img)
+			.setFooter(response.alt);
+
+		message.channel.send(exampleEmbed);
+	}
+}
+
 class XKCD extends Command {
 	constructor(obj: any) {
 		super(obj);
 	}
 
-	post(message, error, response): void {
-		if (error) {
-			console.error(error);
-		} else {
-			const exampleEmbed = new MessageEmbed()
-				.setColor("GOLD")
-				.setTitle(response.title)
-				.setImage(response.img)
-				.setFooter(response.alt);
-
-			message.channel.send(exampleEmbed);
-		}
-	}
 	execute(_client: Client, message: Message, args: Array<string>): void {
 		let arg_string = message.content.slice(5); //Chop off $xkcd
 		let comic_id = arg_string.trim();
 
 		if (comic_id === "") {
 			xkcd.latest(function(error, response) {
-				this.post(message, error, response);
+				post(message, error, response);
 			});
 		} else if (UTIL.isInt(comic_id)) {
 			xkcd.get(comic_id, function(error, response) {
-				this.post(message, error, response);
+				post(message, error, response);
 			});
 		} else {
 			xkcd.random(function(error, response) {
-				this.post(message, error, response);
+				post(message, error, response);
 			});
 		}
 	}
